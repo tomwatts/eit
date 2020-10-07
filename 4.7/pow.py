e92bc3cbd23698af7631d39ef7c624351cdf05cb
Tom <github@tomwatts.ca>
2020-10-07 12:38:27 -0400

    Adding recursive solution and renaming original solution to avoid conflict with built-in pow.

--- 4.7/pow.py
+++ 4.7/pow.py
@@ -1,4 +1,4 @@
-def pow(x, y):
+def my_pow(x, y):
     if y == 0:
         return 1
 
@@ -8,8 +8,26 @@ def pow(x, y):
 
     return result
 
-assert(pow(7, 0) == 1)
-assert(pow(0, 0) == 1)
-assert(pow(2, 4) == 16)
-assert(pow(8, 8) == 16777216)
-assert(pow(100, 4) == 100000000)
+
+def pow_rec(x, y):
+    if y == 0:
+        return 1
+    elif y == 1:
+        return x
+    elif y == 2:
+        return x * x
+    else:
+        return x * pow_rec(x, y - 1)
+
+
+assert(my_pow(7, 0) == 1)
+assert(my_pow(0, 0) == 1)
+assert(my_pow(2, 4) == 16)
+assert(my_pow(8, 8) == 16777216)
+assert(my_pow(100, 4) == 100000000)
+
+assert(pow_rec(7, 0) == 1)
+assert(pow_rec(0, 0) == 1)
+assert(pow_rec(2, 4) == 16)
+assert(pow_rec(8, 8) == 16777216)
+assert(pow_rec(100, 4) == 100000000)
