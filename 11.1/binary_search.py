e7024d295b9cfab31ee068835f3bdeedf5de82b6
Tom <github@tomwatts.ca>
2020-10-26 14:01:46 -0400

    [11.1] Minor refactoring and clean up for brevity.

--- 11.1/binary_search.py
+++ 11.1/binary_search.py
@@ -1,22 +1,19 @@
 def get_first(arr, key):
     l = 0
     u = len(arr) - 1
+    occurrences = []
 
     while (l <= u):
         m = (u + l) // 2
 
-        if arr[m] == key:
-            left_m = get_first(arr[0:m], key)
-            if left_m >= 0:  # Found another in the left side
-                return left_m
-            else:
-                return m
-        elif arr[m] < key:
+        if arr[m] < key:
             l = m + 1
         else:
+            if arr[m] == key:
+                occurrences.append(m)
             u = m - 1
 
-    return -1
+    return min(occurrences) if occurrences else -1
 
 
 assert get_first([-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], 108) == 3
